4b20bafc745fd7d99fde841d7625d972025a420d
Kazeem Jimoh <kazeem0825@gmail.com>
2024-08-22 14:02:48 +0100

    remove space

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.useLogger(app.get(Logger));
 
   await app.listen(3001);
